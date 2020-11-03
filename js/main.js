6bab7d6c542a007f3e0a1c1f9abf99f3322c5835
Arseni <arseni.skobelev@gmail.com>
2020-11-03 21:12:48 +0100

    Main logic implemented. JS, HTML and CSS.

---
+++ js/main.js
@@ -0,0 +1,66 @@
+const canvas = document.getElementById("mainCanvas");
+const ctx = canvas.getContext("2d");
+const increaseBtn = document.getElementById("increaseBtn");
+const decreaseBtn = document.getElementById("decreaseBtn");
+const brushSize = document.getElementById("sizeBrush");
+
+let size = 30;
+let x = 50;
+let y = 50;
+
+var isPressed = false;
+
+canvas.addEventListener("mousedown", () => {
+    isPressed = true;
+});
+
+canvas.addEventListener("mouseup", () => {
+    isPressed = false;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+    x = e.offsetX;
+    y = e.offsetY;
+
+    if (isPressed == true) {
+        drawCircle(x, y);
+    }
+});
+
+function updateSizeOnScreen() {
+    brushSize.innerText = size;
+}
+
+increaseBtn.addEventListener("click", () => {
+    if (size < 50) {
+        size += 5;
+    } else {
+        size = 50;
+    }
+
+    updateSizeOnScreen();
+});
+
+decreaseBtn.addEventListener("click", () => {
+    if (size > 5) {
+        size -= 5;
+    } else {
+        size = 5;
+    }
+
+    updateSizeOnScreen();
+});
+
+function decreaseSz() {
+    if (size >= 50) {
+        size -= 5;
+    }
+}
+
+function drawCircle(x, y) {
+    ctx.beginPath(); 
+    ctx.arc(x, y, size, 0, Math.PI * 2);
+    ctx.fill();
+}
+
+// drawCircle(x, y);
